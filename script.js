0ae85485ce3db00c9f03f27a2d65c4be20b7334e
John Lewis <johnelewis92@gmail.com>
2022-02-16 01:18:47 +0000

    working

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 //our weather api key : e020912d9981563e01ad44d9d285749f
 // link to get current weather:  https://openweathermap.org/current
-// https://imdb-api.com/ example, put this in a function with its own name
-//fetch uses promise syntax to send requests to a server, below is the url of what it is trying to pull
-fetch("https://imdb8.p.rapidapi.com/auto-complete?q=dexter")
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
-//fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}")
-//.then(response => {
-//	console.log(response);
 //Global Variables here
 
 //section 1 - Jaya
@@ -28,12 +16,20 @@ let blue = fetch("https://api.openweathermap.org/data/2.5/weather?lat=21.3131&lo
 
 //section 2 - Justin
 function theWeather(){
-fetch("https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=e020912d9981563e01ad44d9d285749f")
-  .then(response => response.json( ))
-  .then(json => console.log(response.json( )))
-  .catch(err => console.error(err))
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=21&lon=158&appid=e020912d9981563e01ad44d9d285749f")
+    .then(checkStatus)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(nope))
 }
 
+function checkStatus(response){
+  if(response.ok){
+    return Promise.resolve(response);
+  }else{
+    return Promise.reject(new Error(response.statusText));
+  }
+}
 
 //section 3 - name here
 
