588dba2262e20d865c2222537b4a7402f73741cc
John Lewis <johnelewis92@gmail.com>
2022-02-16 01:18:05 +0000

    justin_working

--- script.js
+++ script.js
@@ -5,10 +5,20 @@
 
 //section 2 - Justin
 function theWeather(){
- const fetchPromise = fetch("https://api.openweathermap.org/data/2.5/weather?lat=21&lon=158&appid=e020912d9981563e01ad44d9d285749f");
-  fetchPromise.then(response => {
-  console.log(response);
-});
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=21&lon=158&appid=e020912d9981563e01ad44d9d285749f")
+    .then(checkStatus)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(nope))
 }
+
+function checkStatus(response){
+  if(response.ok){
+    return Promise.resolve(response);
+  }else{
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
 
 
